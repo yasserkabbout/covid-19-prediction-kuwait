b0ecc435363fc5f6c6b1658c91511876c1a99238
Yasser El Kabbout <yelkabbout@talentscript.com>
2020-05-13 02:34:52 +0300

    project python file and CSV data

---
+++ CovidPredictionKuwait.py
@@ -0,0 +1,85 @@
+# importing the required libraries
+import pandas as pd
+
+# Visualisation libraries
+import matplotlib.pyplot as plt
+#import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+import folium 
+from folium import plugins
+from fbprophet import Prophet
+
+# Manipulating the default plot size
+plt.rcParams['figure.figsize'] = 10, 12
+
+
+df = pd.read_csv('covid_19_clean_complete_kuwait.csv',parse_dates=['Date'])
+df.rename(columns={'ObservationDate':'Date', 'Country/Region':'Country'}, inplace=True)
+
+#df_confirmed = pd.read_csv("time_series_covid19_confirmed_kuwait.csv")
+#df_recovered = pd.read_csv("time_series_covid19_recovered_global.csv")
+#df_deaths = pd.read_csv("time_series_covid19_deaths_global.csv")
+
+#df_confirmed.rename(columns={'Country/Region':'Country'}, inplace=True)
+#df_recovered.rename(columns={'Country/Region':'Country'}, inplace=True)
+#df_deaths.rename(columns={'Country/Region':'Country'}, inplace=True)
+
+# df2 = df.groupby(["Date", "Country", "Province/State"])[['Date', 'Province/State', 'Country', 'Confirmed', 'Deaths', 'Recovered']].sum().reset_index()
+
+confirmed = df.groupby('Date').sum()['Confirmed'].reset_index()
+deaths = df.groupby('Date').sum()['Deaths'].reset_index()
+recovered = df.groupby('Date').sum()['Recovered'].reset_index()
+casesPerDay = df.groupby('Date').sum()['CasesPerDay'].reset_index()
+recoveredPerDay = df.groupby('Date').sum()['RecoveredPerDay'].reset_index()
+deathPerDay = df.groupby('Date').sum()['DeathPerDay'].reset_index()
+activeCases = df.groupby('Date').sum()['ActiveCases'].reset_index()
+italyCasesPerDay = df.groupby('Date').sum()['ItalyCasesPerDay'].reset_index()
+
+fig = go.Figure()
+# # #Plotting datewise confirmed cases
+# fig.add_trace(go.Scatter(x=confirmed['Date'], y=confirmed['Confirmed'], mode='lines+markers', name='Confirmed',line=dict(color='blue', width=2)))
+# fig.add_trace(go.Scatter(x=deaths['Date'], y=deaths['Deaths'], mode='lines+markers', name='Deaths', line=dict(color='Red', width=2)))
+# fig.add_trace(go.Scatter(x=recovered['Date'], y=recovered['Recovered'], mode='lines+markers', name='Recovered', line=dict(color='Green', width=2)))
+#fig.add_trace(go.Scatter(x=casesPerDay['Date'], y=casesPerDay['CasesPerDay'], mode='lines+markers', name='CasesPerDay',line=dict(color='orange', width=2)))
+
+fig.add_trace(go.Bar(x=casesPerDay['Date'], y=casesPerDay['CasesPerDay']))
+#fig.add_trace(go.Bar(x=italyCasesPerDay['Date'], y=italyCasesPerDay['ItalyCasesPerDay']))
+
+
+fig.update_layout(title='Kuwait NCOVID-19 Cases', xaxis_tickfont_size=14,yaxis=dict(title='Number of Active Cases'))
+fig.show()
+
+
+
+confirmed.columns = ['ds','y']
+deaths.columns =['ds','y']
+recovered.columns=['ds','y']
+casesPerDay.columns=['ds','y']
+recoveredPerDay.columns = ['ds','y']
+deathPerDay.columns = ['ds','y']
+activeCases.columns = ['ds','y']
+italyCasesPerDay.columns = ['ds','y']
+
+
+confirmed['ds'] = pd.to_datetime(confirmed['ds'])
+deaths['ds'] = pd.to_datetime(deaths['ds'])
+recovered['ds'] = pd.to_datetime(recovered['ds'])
+casesPerDay['ds']=pd.to_datetime(casesPerDay['ds'])
+recoveredPerDay['ds'] = pd.to_datetime(recoveredPerDay['ds'])
+deathPerDay['ds'] = pd.to_datetime(deathPerDay['ds'])
+activeCases['ds'] = pd.to_datetime(activeCases['ds'])
+italyCasesPerDay['ds'] = pd.to_datetime(italyCasesPerDay['ds'])
+
+m = Prophet(mcmc_samples=300)
+#m = Prophet(interval_width=0.95)
+m.fit(casesPerDay)
+#m.fit(italyCasesPerDay)
+future = m.make_future_dataframe(periods=20)
+future.tail()
+
+#predicting the future with date, and upper and lower limit of y value
+forecast = m.predict(future)
+forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+m.plot(forecast)
+plt.show()
